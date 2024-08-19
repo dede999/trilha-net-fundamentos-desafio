c9afd33810102909a088249af372e97d5fda1551
Andre Luiz Abdalla Silveira <andreluizsa00@gmail.com>
2024-08-19 16:38:34 -0300

    feat: Implement method to display parking time

--- DesafioFundamentos/Models/Carros.cs
+++ DesafioFundamentos/Models/Carros.cs
@@ -10,4 +10,15 @@ public class Carros
         Placa = placa;
         Entrada = DateTime.Now;
     }
+    
+    public void MostraTempoEstacionado()
+    {
+        // Calcula o tempo que o carro ficou estacionado.
+        // Para que o preço final não fique baixo, vou fazer
+        // com que os segundos sejam representados como minutos
+        // e os minutos como horas
+        var minutos = (DateTime.Now - Entrada).Seconds;
+        var horas = (DateTime.Now - Entrada).Minutes;
+        Console.WriteLine($"{Placa}\t{horas}:{minutos}");
+    }
 }
